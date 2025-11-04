fc8d2c2e8a68801049df9f289875f1f040691c04
ADARSH P <adarshprabha7512@gmail.com>
2025-11-04 23:17:35 +0530

    Refactor Bencode Cipher and add statistics
    Refactor Bencode Cipher functions and add statistics features.

--- Assignment/Bencode-Cipher/bencode_cipher.js
+++ Assignment/Bencode-Cipher/bencode_cipher.js
@@ -1,167 +1,132 @@
-function encode(input) {
-  switch (typeof input) {
-    case "number":
-      return 'i' + input + 'e';
-    case "string":
-      return input.length + ":" + input;
-    case "object":
-      return "l" + addString(input) + "e";
-  }
-}
+let numberOfIteration = 0; // global variable
 
-function decode(input) {
-  if (input[0] === "i") {
-    return parseInt(input.slice(1,input.length - 1));
-  }
-
-  if (input[0] === "l") {
-    return decodeArray(input.slice(1,input.length - 1));
-  }
-
-  if (typeof parseInt(input[0]) === "number") {
-    return input.slice(input.indexOf(":") + 1, input.length);
-  }
-  
-}
-
-function decodeArray(input) {
-  const array = [];
-
-  if (input.length !== 0) {
-    const slicedValue = decode(input);
-    array.push(slicedValue);
-  }
-
-  return array;
-}
-
-function pushElements(input, array) {
-  return array.push(input);
-}
-
-function addString(input) {
-  let concatString = "";
-
-  if (input.length !== 0) {
-    
-    for (let index = 0; index < input.length; index++) {
-      concatString += encode(input[index]);
-    }
-  }
-
-  return concatString;
-}
+// ....................................... underline the heading .............................................
 
 function underline(content) {
   return content + "\n" + "-".repeat(content.length);
 }
 
-function emoji(actual, expected) {
-  return areEqual(actual, expected) ? "✅" : "🚫";
-}
+// ....................................... sorting the input data .............................................
 
-function isElementsEqual(actual, expected) {
-  for (let index = 0; index < actual.length; index++) {
-    if (actual[index] !== expected[index]) {
-      return false;
+function sortData(data) {
+  for (let i = 0; i < data.length; i++) {
+    for (let j = i + 1; j < data.length; j++) {
+      numberOfIteration++;  // increment operation 
+      if (data[i] > data[j]) {
+        const max = data[j];
+        data[j] = data[i];
+        data[i] = max;
+      }
     }
   }
-
-  return true;
+  return data;   // returns the sorted data by modifiying the original array 
 }
 
-function areEqual(actual, expected) {
-  const isLengthEqual = actual.length === expected.length;
-  return isLengthEqual && isElementsEqual(actual, expected);
+// .................................... generates random data ..................................................
+
+function randomData(lower, upper) {
+  return lower + Math.round(Math.random() * (upper - lower));
 }
 
-function displayFailingCase(input, actual, expected) {
-  if (!areEqual(actual, expected)) {
-    console.log("  ↠  Input : ", input);
-    console.log("  ↠  Actual Output : ", actual);
-    console.log("  ↠  Expected Output : ", expected);
+function generateRandomData(numberOfElements, lower, upper) {
+  const dataArray = [];
+
+  for (let index = 0; index < numberOfElements; index++) {
+    dataArray.push(randomData(lower, upper));
   }
+
+  return dataArray;
 }
 
-function displayOutput(actual, desc, input, expected) {
-  const finalEmoji = emoji(actual, expected);
+// ............................................. bench mark ....................................................
 
-  console.log(finalEmoji, desc);
-
-  return displayFailingCase(input, actual, expected);
+function benchmark(data) {
+  return `size : ${data.length} \nnumber of iteration : ${numberOfIteration}`;
 }
 
-function testEncode(desc, input, expected) {
-  const actual = encode(input);
-  return displayOutput(actual, desc, input, expected);
+// ............................................ mean value ....................................................
+
+function mean(sortedData) {
+  let sum = 0;
+
+  for (let index = 0; index < sortedData.length; index++) {
+    sum += sortedData[index];
+  }
+  return sum / sortedData.length;
 }
 
-function testDecode(desc, input, expected) {
-  const actual = decode(input);
-  return displayOutput(actual, desc, input, expected);
+//  ...........................................median value .....................................................
+
+function median(data) {
+  const sortedData = sortData(data);
+  console.log(sortedData);
+  const index = (sortedData.length - 1) / 2;
+  return (sortedData[Math.ceil(index)] + sortedData[Math.floor(index)]) / 2;
 }
 
-function testAll() {
-  console.log(underline("Bencode Cipher Encode"));
-  testEncodeNumber();
-  testEncodeString();
-  testEncodeArray();
-  testEncodeNestedArray();
+// ................................................ deviation ..................................................
 
-  console.log("\n");
-  console.log(underline("Bencode Cipher Encode"));
-  testDecodeNumber();
-  testDecodeString();
-  testDecodeArray();
-  // // testDecodeNestedArray();
+function deviation(data) {
+  const array = [];
+  const meanVal = mean(data);
+  display("mean", meanVal);
+
+  for (let index = 0; index < data.length; index++) {
+    array.push(data[index] - meanVal);
+  }
+
+  return array;
 }
 
-function testEncodeNestedArray() {
-  testEncode("nested array", ["apple", 123, ["banana", -5]], "l5:applei123el6:bananai-5eee");
-  testEncode("nested array", [0, "", ["test"]], "li0e0:l4:testee");
-  testEncode("nested array", ["", 0, []], "l0:i0elee");
-  testEncode("nested array", ["one", ["two", ["three"]]], "l3:onel3:twol5:threeeee");
+// ....................................... standard deviation ..................................................
+
+function standardDeviation(data) {
+  return calculate(deviation(data));
 }
 
-function testEncodeArray() {
-  testEncode("array", [], "le");
-  testEncode("array", [1], "li1ee");
+// .................................. calculate standard deviation  .............................................
+
+function calculate(absVal) {
+  let sum = 0;
+
+  for (let index = 0; index < absVal.length; index++) {
+    const sqVal = Math.pow(absVal[index], 2);
+    sum += sqVal;
+  }
+
+  return Math.sqrt(sum / absVal.length);
 }
 
-function testEncodeString() {
-  testEncode("string", "", "0:");
-  testEncode("string", "hello world", "11:hello world");
-  testEncode("string", "special!@#$chars", "16:special!@#$chars");
+// ...............................................main function ................................................
+
+function statistics() {
+  // const data = generateRandomData(numberOfElements, lower, upper);
+  const heading = prompt("Heading :");
+  const numberOfElements = parseInt(prompt("enter the number of elements : "));
+  const data = generateDataArray(numberOfElements);
+  // const data = [0, 0 , 0 , 100];
+  // console.log(benchmark(data));
+
+  console.log(underline(`Statistics of ${heading}`));
+  display("median", median(data));
+  display("standard Deviation", standardDeviation(data));
 }
 
-function testEncodeNumber() {
-  testEncode("Number", 123, "i123e");
-  testEncode("number", -42, "i-42e");
-  testEncode("number", 0, "i0e");
+function generateDataArray(numberOfElements) {
+  const data = [];
+
+  for (let index = 0; index < numberOfElements; index++) {
+    const element = parseInt(prompt("enter data"));
+    data.push(element);
+  }
+
+  console.clear();
+  return data;
 }
 
-// function testDecodeNestedArray() {
-//   testDecode("nested array", "l5:applei123el6:bananai-5eee", ["apple", 123, ["banana", -5]]);
-//   testDecode("nested array", "li0e0:l4:testee", [0, "", ["test"]]);
-//   testDecode("nested array", "l0:i0elee", ["", 0, []]);
-//   testDecode("nested array", "l3:onel3:twol5:threeeee", ["one", ["two", ["three"]]]);
-// }
 
-function testDecodeArray() {
-  testDecode("array", "le", []);
-  testDecode("array", "li1ee", [1]);
+function display(string, data) {
+  console.log("🟣", string,"  :  ",data);
 }
 
-function testDecodeString() {
-  testDecode("string", "0:", "");
-  testDecode("string", "11:hello world", "hello world");
-  testDecode("string", "16:special!@#$chars", "special!@#$chars");
-}
-
-function testDecodeNumber() {
-  testDecode("Number", "i123e", 123);
-  testDecode("number", "i-42e", -42);
-  testDecode("number", "i0e", 0);
-}
-
-testAll();
+statistics();
